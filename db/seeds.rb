cb340f7cf9995d0ff0a84c6157734ded18bfaa73
Vladyslav Vorobei <v.vorobei@mobidev.biz>
2021-06-18 21:43:30 +0300

    The seed script is now adding for each created category and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# run using: rails db:seed
+User.destroy_all
+Category.destroy_all
+
+NUMBER_OF_CATEGORIES = ["Surgeon", "Oculist", "Dermatologist", "Dentist", "Psychiatrist"]
+
+for category_name in NUMBER_OF_CATEGORIES do
+    category = Category.create(name: category_name)
+
+    number_of_users = rand(5..10)
+    number_of_users.times do
+        category.users.create(name: Faker::Name.unique.name_with_middle)
+    end 
+end
+
+puts "Created #{Category.count} Categories."
+puts "Created #{User.count} users."
