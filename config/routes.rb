8598806906f3152242be2fbc36ca58c8823bb3bf
Vladyslav Vorobei <v.vorobei@mobidev.biz>
2021-06-20 14:28:32 +0300

    home button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'categories#index'
-  get "categories/:id", to: "categories#show", id: /\d+/
+  get "categories/:id", to: "categories#show", id: /\d+/, as: 'category'
   
 end
