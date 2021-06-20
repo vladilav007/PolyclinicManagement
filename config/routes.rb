c576af0fe7c838c74f81beb240e553fb9b26f927
Vladyslav Vorobei <v.vorobei@mobidev.biz>
2021-06-20 14:46:00 +0300

    added a users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root to: 'categories#index'
+  get 'categories', to: 'categories#index'
   get "categories/:id", to: "categories#show", id: /\d+/, as: 'category'
   
 end
